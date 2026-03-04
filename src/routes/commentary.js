438955b1486e7f418b6195e61d8248de87f2cf4c
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-03-04 13:05:24 +0600

    feat: add commentary broadcasting functionality to WebSocket server

--- src/routes/commentary.js
+++ src/routes/commentary.js
@@ -78,6 +78,10 @@ commentaryRouter.post('/', async (req, res) => {
             ...rest
         }).returning();
 
+        if(res.app.locals.broadcastCommentary) {
+            res.app.locals.broadcastCommentary(result.matchId, result);
+        }
+        
         res.status(201).json({ 
             data: result
         });
